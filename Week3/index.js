9d7a1888a8da52a50e558aa0ca71e56054e2a42b
Moiz Din <moizdin1@gmail.com>
2024-12-10 07:44:45 +0000

    Add files via upload

--- Week3/index.js
+++ Week3/index.js
@@ -1,38 +1,113 @@
-//definition of the function EmployeeInfo
-function EmployeeInfo(name, Salary) {
-    console.log("Welcome " + name + " Your monthly Salary is " + Salary);
-}
-console.log("This is my first program");
-var EmpName = "John";
-var EmpSalary = 50000;
-//calling the function
-EmployeeInfo(EmpName, EmpSalary);
+const mongoose = require('mongoose');
+//const app = express();
 
-const EmpSkills= (skills)=> {
-    console.log("Expert in "+ skills)
-   }
-   EmpSkills("java")
+const MONGO_URI = 'mongodb://localhost:27017/Week8';
 
-const student= require('./StudentInfo')
-const person = require('./Person')
-console.log("Student Name:" +student.getName())
-console.log(student.Location())
-console.log(student.dob)
-console.log(student.Studentgrade())
-console.log("grade is "+student.Studentgrade(55))
-person1= new person("Jim","USA","myemail@gmail.com")
-console.log("using Person Module", person1.getPersonInfo())
-console.log("Programe ended")
+//Connect to MongoDB
+mongoose.connect(MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 
-os=require("os")
-const util=require('util')
-console.log("temporary directory"+ os.tmpdir() )
-console.log("hostname: "+ os.hostname())
-console.log("OS : " + os.platform() +"release:"+ os.release())
-console.log("Uptime"+ (os.uptime())/3600 +" hours")
-console.log("userInfo" + util.inspect(os.userInfo()))
-console.log("Memory "+ os.totalmem()/1000000000 + "Giga byte")
-console.log(" free: "+os.freemem()/1000000000 + "Giga byte")
-console.log("CPU "+ util.inspect(os.cpus()))
-console.log("Network"+ util.inspect(os.networkInterfaces()))
-console.log("programe end")
+const db = mongoose.connection;
+
+//Handle connection errors
+db.on('error', function (err) {
+    console.log("Error occurred during connection: " + err);
+});
+
+//Once connected
+db.once('connected', function () {
+    console.log(`Connected to ${MONGO_URI}`);
+});
+
+//Creating the schema
+const PersonSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    age: Number,
+    gender: String,
+    salary: Number
+});
+
+const personDoc = mongoose.model('Person', PersonSchema, 'personCollection');
+
+ const doc1 = new personDoc({
+    name: 'Mo',
+    age: 20,
+    gender: "Male",
+    salary: 5000
+});
+
+ doc1.save()
+    .then((doc1) => {
+        console.log("New document has been added into your database.", doc1);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+let manypersons = [
+    { name: 'Simon', age: 42, gender: "Male", salary: 3456 },
+    { name: 'Neesha', age: 23, gender: "Female", salary: 1000 },
+    { name: 'Mary', age: 27, gender: "Female", salary: 5402 },
+   { name: 'Mike', age: 40, gender: "Male", salary: 4519 }
+];
+
+personDoc.insertMany(manypersons)
+   .then(function() {
+        console.log("Data inserted"); // Success
+    })
+    .catch(function(error) {
+        console.log(error); // Failure
+    });
+
+personDoc.find({})
+         .sort({salary: 1})
+        .select('name Salary age')
+        .limit(10)
+        .exec()
+        .then(docs => {
+            console.log("showing multiple documents")
+            docs.forEach(function(Doc) {
+                console.log(Doc.age, Doc.name);
+        })
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+var givenage = 15 
+personDoc.find({gender:"Female", age:{$gte:givenage}})
+            .sort({salary:1})
+            .select('name Salary age')
+            .limit(10)
+            .exec()
+            .then(docs => {
+            console.log("showing age greater than", givenage)
+            docs.forEach(function(Doc) {
+            console.log(Doc.age, Doc.name)
+            })
+            })
+            .catch(err => {
+            console.error(err)})
+
+personDoc.countDocuments().exec()
+.then(count=>{
+console.log("Total documents Count :", count)
+}) .catch(err => {
+console.error(err)
+})
+            
+personDoc.deleteMany({ age: { $gte: 25 } })
+.exec()
+.then(docs=>{
+console.log('deleted documents are:',docs);
+}).catch(function(error){
+console.log(error);
+});
+
+personDoc.updateMany({ gender: "Female" },{salary:5555})
+.exec()
+.then(docs=>{
+console.log("update")
+console.log(docs); 
+}).catch(function(error){
+console.log(error); 
+});
