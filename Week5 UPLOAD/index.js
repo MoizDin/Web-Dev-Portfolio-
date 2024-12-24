15c33cbb22b7dc10a3fbbfcfb5f73ff7dea1d728
Moiz Din <moizdin1@gmail.com>
2024-12-24 19:26:59 +0000

    Add files via upload

---
+++ Week5 UPLOAD/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GreetingElement from './mygreetingapp'
+import AppColor from './appbackgroundcolor'
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+ <React.StrictMode>
+ <AppColor heading="This is first element" lbl="Name :" color="green"/>
+ <AppColor heading="This is second element" lbl="Name :" color="blue"/>
+ <AppColor heading="This is third third element" lbl="Name :" color="Yellow"/>
+ </React.StrictMode>,
+ document.getElementById('root')
+);
+reportWebVitals();
